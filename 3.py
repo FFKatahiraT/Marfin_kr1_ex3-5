fcb6e700e2ec71d27ee13c71019166c1b9aea381
Tim <58525817+FFKatahiraT@users.noreply.github.com>
2021-10-29 18:15:56 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+def read_file(name):
+	file = open(name+'.csv', 'r')
+	data = file.read().strip().split('\n')
+	data = data[2:]
+	t, Q0, Q, P= [], [], [], []
+	for line in data:
+		elements = line.split(',')
+		t.append(float(elements[0]))
+		Q0.append(float(elements[1])*10**-6)
+		Q.append(float(elements[2])*10**-6)
+		P.append(float(elements[3])*10**6)
+	return t, Q0, Q, P
+
+def plotter(x, y, x_label, name):
+	linestyle=['solid', 'dotted', 'dashed', 'dashdot', (0, (5,10)), (0, (5,1)), (0, (3,10,1,10)), (0, (3,1,1,1))]
+	for i in range(len(y)):
+		plt.scatter(x, y[i], linestyle=linestyle[i])
+	plt.title(name)
+	plt.grid()
+	plt.ylabel(str(name))
+	plt.xlabel(x_label)
+	plt.tight_layout()
+	plt.savefig(str(name)+str('.png'))
+	plt.close()
+
+t, Q0, Q, P = read_file('3')
+tau1 = 5000
+tau2 = 1000
+P_der_Q, exp_P_der_Q, log_t = [], [], []
+for i in range(len(P)):
+	P_der_Q.append(P[i]/(Q0[i]-Q[i]))	#dPc/(Q0-Q))=1/(4 pi eps)*(lnt+ln2.25*psi/rc**2)
+	log_t.append(math.log(t[i]))
+lenght = len(t)//8*7
+k, b = np.polyfit(log_t[lenght:], P_der_Q[lenght:], 1)	#k=1/(4 pi eps), b=1/(4 pi eps) ln (2.25psi/rc**2)
+eps = 1/(4*3.14*k)
+hiStar_rc2 = math.exp(b*4*3.14*eps)/2.25
+hi_rc2 = hiStar_rc2*(tau1+tau2)/tau1
+
+plotter(t[lenght:], [P[lenght:]], r'$t$ [s]', r'$dP_c$($t$) [Pa]')
+plotter(log_t[lenght:], [P_der_Q[lenght:]], r'$ln(t)$', r'$\frac{dP_c(t)}{Q_0-Q(t)}$')
+print(eps, 'epsilon [m^3/Pa*s]')
+print(hiStar_rc2, 'hi*/r_c^2 [1/s]')
+print(hi_rc2, 'hi/r_c^2 [1/s]')
---
+++ 5.py
@@ -0,0 +1,70 @@
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+def read_file(name):
+	file = open(name+'.csv', 'r')
+	data = file.read().strip().split('\n')
+	data = data[2:]
+	output = [[], [], [], []]
+	for line in data:
+		elements = line.split(',')
+		for i in range(len(elements)):
+			output[i].append(float(elements[i]))
+	return output
+
+def plotter(x, y, x_label, name):
+	linestyle=['solid', 'dotted', 'dashed', 'dashdot', (0, (5,10)), (0, (5,1)), (0, (3,10,1,10)), (0, (3,1,1,1))]
+	for i in range(len(y)):
+		plt.plot(x, y[i], label=name[i],linestyle=linestyle[i])
+	plt.title(name[0]+'&'+name[1])
+	plt.grid()
+	plt.legend(loc='best')
+	# plt.ylabel(str(name))
+	plt.xlabel(x_label)
+	plt.tight_layout()
+	plt.savefig(str(name)+str('.png'))
+	plt.close()
+
+def find_max(func):
+	t_qmax= []
+	func0=max(func)
+	for i in range(len(func)):
+		if func[i] == func0:
+			t_qmax.append(t[i])
+	return func0, t_qmax
+
+r = 420
+gamma = 1.781
+t, q, Pc, Pr = read_file('5')
+for i in range(len(t)):
+	q[i] = q[i]*10**-6
+	Pc[i] = Pc[i]*10**6
+	Pr[i] = Pr[i]*10**6
+
+deltaT=[]
+q0, t_qmax = find_max(q)
+for i in range(len(t_qmax)-1):
+	deltaT.append(t_qmax[i+1]-t_qmax[i])
+
+t_Pcmax = find_max(Pc)[1]
+t_Pcmax_av = sum(t_Pcmax)/len(t_Pcmax)
+delta_qc = 6.28*(t_Pcmax_av-t_qmax[0])/deltaT[0]
+
+Pr0, t_Prmax = find_max(Pr)
+t_Prmax_av = sum(t_Prmax)/len(t_Prmax)
+delta_qr = 6.28*(t_Prmax_av-t_qmax[0])/deltaT[0]
+
+hi = 3.14*r**2/(deltaT[0]*(delta_qr-3.14/8)**2)
+eps = q0/(2**(7/2)*3.14**0.5*Pr0)*math.exp(-(delta_qr-3.14/8))/(delta_qr-3.14/8)**0.5
+rc = r*2**0.5/gamma*math.exp(-3.14/(4*math.tan(delta_qc)))/(delta_qr-3.14/8)
+
+lenght = 0
+plotter(t[lenght:], [Pr[lenght:], Pc[lenght:]], r'$t$ [s]', [r'$P_r$($t$) [Pa]', r'$P_c$($t$) [Pa]'])
+print(q0, 'q0')
+print(t_qmax, 't при q0')
+print(deltaT, 'Период колебаний T')
+print(delta_qr, 'Разность фаз дебит-давление [rad]')
+print(hi, 'hi [m^2/s]')
+print(eps, 'eps [m^3/(Pa*s)]')
+print(rc, 'rc [m]')
